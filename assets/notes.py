0f15fbd2a6a86954c6645754abe842d9d39dbfc9
Dabson <cyphlix@gmail.com>
2018-06-22 16:52:14 +1000

    Init the bot

---
+++ assets/notes.py
@@ -0,0 +1,12 @@
+Sentdex
+https://www.youtube.com/watch?v=v3LJ6VvpfgI&t=225s
+
+gotta change sc2/paths.py to your starcraft2 install location
+initial 
+BASEDIR = {
+    "Windows": "C:/Program Files (x86)/StarCraft II",
+
+Changed to 
+  "F:/Media/Games/Blizzard/StarCraft II"
+
+realtime false | true
