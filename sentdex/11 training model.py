359d65a428273fea740dbe43a92a384710395f77
Dabson <cyphlix@gmail.com>
2018-07-15 11:34:12 +1000

    do some train

--- sentdex/11 training model.py
+++ sentdex/11 training model.py
@@ -40,3 +40,65 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 tensorboard = TensorBoard(log_dir='logs/stage1')
+train_data_dir="10train_data"
+
+def check_data():
+  choices = {"no_attacks": no_attacks,
+             "attack_closest_to_nexus": attack_closest_to_nexus,
+             "attack_enemy_structures": attack_enemy_structures,
+             "attack_enemy_start": attack_enemy_start}
+
+  total_data = 0
+  lengths = []
+  for choice in choices: 
+    print("Length of {} is: {}".format(choice, len(choices[choice])))
+    total_data += len(choices[choice])
+    lengths.append(len(choices[choice]))
+
+  print("Total data length now is:", total_data)
+  return lengths
+
+hm_epochs = 10 
+for i in range(hm_epochs):
+  current = 0
+  increment = 200 
+  not_maximum = True
+  all_files = os.listdir(train_data_dir)
+  maximum = len(all_files)
+  random.shuffle(all_files)
+
+  while not_maximum: 
+    print("currently doing {}:{}".format(current,current+increment))
+    no_attacks = []
+    attack_closest_to_nexus = []
+    attack_enemy_structures = []
+    attack_enemy_start = []
+    for file in all_files[current:current+increment]:
+      full_path = os.path.join(train_data_dir, file)
+      data = np.load(full_path)
+      data = list(data)
+      for d in data:
+        choice = np.argmax(d[0])
+        if choice == 0:
+          no_attacks.append(d)
+        elif choice == 1:
+          attack_closest_to_nexus.append(d)
+        elif choice == 2:
+          attack_enemy_structures.append(d)
+        elif choice == 3:
+          attack_enemy_start.append(d)
+
+    lengths = check_data()
+    lowest_data = min(lengths)
+
+    random.shuffle(no_attacks)
+    random.shuffle(attack_closest_to_nexus)
+    random.shuffle(attack_enemy_structures)
+    random.shuffle(attack_enemy_start)
+
+    no_attacks = no_attacks[:lowest_data]
+    attack_closest_to_nexus = attack_closest_to_nexus[:lowest_data]
+    attack_enemy_structures = attack_enemy_structures[:lowest_data]
+    attack_enemy_start = attack_enemy_start[:lowest_data]
+
+    check_data()
